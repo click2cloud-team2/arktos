059679b4e43a2fc0cdcb44c8018a2d96a96f16f7
Click2cloud-gamma1 <click2cloud-gamma1@click2cloud.net>
2021-10-01 16:35:02 +0530

    optimized setup-dev-node script and set arktos-cni.rc variable

--- hack/setup-dev-node.sh
+++ hack/setup-dev-node.sh
@@ -16,44 +16,32 @@
 
 # Convenience script to setup a fresh Linux installation for Arktos developers.
 
-function set_profile_path {
-  echo export PATH=$PATH:/usr/local/go/bin\ >> ~/.profile
-  source ~/.profile
-}
-
-function install_dockerio {
-  sudo apt -y update
-  sudo apt -y install docker.io
-}
-
-function install_make {
-  sudo apt -y update
-  sudo apt -y install make
-}
-
-function install_gcc {
-  sudo apt -y update
-  sudo apt -y install gcc
-}
-
-function install_jq {
-  sudo apt -y update
-  sudo apt -y install jq
-}
+set -o errexit
+set -o nounset
+set -o pipefail
 
 function install_golang {
   GOLANG_VERSION=${GOLANG_VERSION:-"1.13.9"}
   wget https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz -P /tmp
   sudo tar -C /usr/local -xzf /tmp/go${GOLANG_VERSION}.linux-amd64.tar.gz
   rm -rf /tmp/go${GOLANG_VERSION}.linux-amd64.tar.gz
-  set_profile_path
+
+  if ! [ -n "$(echo $PATH |grep 'go')" ]; then
+    echo export PATH=$PATH:/usr/local/go/bin\ >> ~/.profile
+    source ~/.profile
+  fi
 }
 
 function check_installed_dependencies {
+  if ! [ -n "$(go version | grep '1.13.9')" ] || ! [ -x "$(command -v make)" ] || ! [ -x "$(command -v gcc)" ] || ! [ -x "$(command -v jq)" ]; then
+    echo "Running... update"
+    sudo apt -y update
+  fi
+
   echo "Check if docker is installed"
   if ! [ -x "$(command -v docker -v)" ]; then
     echo 'Error: docker is not installed.' >&2
-    install_dockerio
+    sudo apt -y install docker.io
   else
     echo "docker is installed"
   fi
@@ -63,14 +51,7 @@ function check_installed_dependencies {
     echo 'Error: go is not installed.' >&2
     install_golang
   else
-    go_version=$(go version | cut -d' ' -f3 | cut -d'o' -f2)
-    major_version=$(echo $go_version | cut -d'.' -f1)
-    major_sub_version=$(echo $go_version | cut -d'.' -f2)
-    minor_sub_version=$(echo $go_version | cut -d'.' -f3)
-
-    if [[ "$major_version" == "1" ]] && [[ "$major_sub_version" == "13" ]] && [[ "$minor_sub_version" == "9" ]]; then
-      echo "golang 1.13.9 is installed"
-    else
+    if ! [ -n "$(go version | grep '1.13.9')" ]; then
       echo "Error: golang 1.13.9 is NOT installed"
       sudo rm -rf /usr/local/go
       install_golang
@@ -80,19 +61,19 @@ function check_installed_dependencies {
   echo "Check if make & gcc & jq are installed"
   if ! [ -x "$(command -v make)" ]; then
     echo "Error: make is not installed"
-    install_make
+    sudo apt -y install make
   else
     echo "make is installed"
   fi
   if ! [ -x "$(command -v gcc)" ]; then
     echo "Error: gcc is not installed"
-    install_gcc
+    sudo apt -y install gcc
   else
     echo "gcc is installed"
   fi
   if ! [ -x "$(command -v jq)" ]; then
     echo "Error: jq is not installed"
-    install_jq
+    sudo apt -y install jq
   else
     echo "jq is installed"
   fi
